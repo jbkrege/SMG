e98744156e22e255ec3b398796d253ed44430888
Clay Kauzlaric <clay.kauzlaric@yahoo.com>
2017-03-11 22:12:50 -0600

    Update granulateTestScript.py

--- granulateTestScript.py
+++ granulateTestScript.py
@@ -2,12 +2,14 @@
 
 from granulate import *
 
-music, sr = librosa.load('misuse.wav')
+music, sr = librosa.load('/path/to/your/sound.wav')
 
+#run with set grain size of 20 ms
 test = granulate(music, sr, 0.02, 80, 1, 20, 'tukey')
 
 librosa.output.write_wav('grantest.wav', test, sr)
 
+#run with random grain sizes between 10 and 100 ms 
 test = granulate(music, sr, 0, 80, 1, 20, 'tukey')
 
 librosa.output.write_wav('grantest2.wav', test, sr)
