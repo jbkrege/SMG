00f6618ccc1133b95e5d22afb886cf2521729895
parwell <pawelklim1@gmail.com>
2017-03-16 21:49:24 -0500

    changing example and undid accidental change

--- ClassExample.py
+++ ClassExample.py
@@ -1,13 +1,13 @@
 from Granulator import *
 
-g = Granulator('birds.wav','fireplace.wav')
+g = Granulator('../sounds/birds.wav','../sounds/fireplace.wav')
 g.Print()
 g.run("../sounds/birds_fireplace.wav")
 
-g.setGrainLen(0.5)
+g.setGrainlen(0.5)
 g.Print()
-g.run("../sounds/birds_fireplace_long_grain.wav")
+g.run("../output/birds_fireplace_long_grain.wav")
 
-g.setGrainLen(0.02)
+g.setGrainlen(0.02)
 g.Print()
-g.run("../sounds/birds_fireplace_short_grain.wav")
+g.run("../output/birds_fireplace_short_grain.wav")
